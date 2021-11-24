87eed34856719c18bb11fb45b412d8d376c96fe9
Faiyaz286 <fahimfaiyaz286@gmail.com>
2021-11-24 15:51:23 +0600

    quiz 3

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/header/header';
+import Hero from './components/hero/hero';
+import Footer from './components/footer/footer';
+import Main from './components/main/main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
